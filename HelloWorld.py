904ba3c19828c0c9df9b85e469d015ea8bbb452b
Jacob Stambaugh <Jstambaugh00@gmail.com>
2021-01-20 15:46:29 -0600

    Added problems 1&2 to HW1

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,5 +1,17 @@
-def main():
+def HelloWorld():
     print("Hello World")
 
+def problem1():
+    # Calculate hypotnuse
+    a=int(input('enter number 1 '))
+    b=int(input('enter number 2 '))
+    c=(a**2+b**2)
+    print("Hypotonuse is:",c)
 
-main()
+def problem2():
+    # String splitting + concat
+    s=input('Enter String ')
+    nums = list(map(int, input("\nEnter the numbers : ").strip().split()))[:4]
+    print(s[nums[0]:nums[1]+1] , s[nums[2]:nums[3]+1])
+
+problem2()
