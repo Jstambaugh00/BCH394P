4854665921ab925a1cc034a9869cdce15919b29d
Jacob Stambaugh <Jstambaugh00@gmail.com>
2021-01-21 15:56:44 -0600

    Solved Counting DNA Nucleotides Porblem

---
+++ Counting DNA Nucleotides.py
@@ -0,0 +1,32 @@
+# Counting DNA Nucleotides
+# Jacob Stambaugh
+# 1/21/2020
+
+# Problem:
+''' A string is simply an ordered collection of symbols selected from some alphabet and formed into a word; the length
+of a string is the number of symbols that it contains. An example of a length 21 DNA string (whose alphabet contains the symbols
+ 'A', 'C', 'G', and 'T') is "ATGCTTCAGAAAGGTCTTACG." Given: A DNA string s of length at most 1000 nt.
+Return: Four integers (separated by spaces) counting the respective number of times that the symbols 'A', 'C', 'G', and 'T' occur in s. '''
+
+# Example string:
+#AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC
+# ans:
+#20 12 17 21
+
+def main():
+    # get used input
+    s=input('Enter DNA strand: ')
+    total=[0,0,0,0]
+    for i in s:
+        if i=='A':
+            total[0]=total[0]+1
+        elif i=='C':
+            total[1]=total[1]+1
+        elif i=='G':
+            total[2]=total[2]+1
+        else:
+            total[3]=total[3]+1
+    print('There are',total, 'A C G T base pairs respectively')
+
+
+main()
